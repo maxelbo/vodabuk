fb902f5717d36113043883a316fd3f1ab406cbff
Max Elbo <hello.maxelbo@gmail.com>
2024-07-01 07:33:18 +0900

    feat(api):add letter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,38 @@
 import Search from '@/components/Search';
-// TEMP: Importing the data from the JSON file
-import dictionaryData from '@/data/dictionaryData.json';
 
 // To fetch from the API
-// async function getDictionaryData() {
-//   const res = await fetch(process.env.API_ENDPOINT!);
+async function getDictionaryData() {
+  const endpoint = process.env.API_ENDPOINT!;
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
+  try {
+    const res = await fetch(endpoint);
 
-//   return res.json();
-// }
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data from ${endpoint}`);
+    }
+
+    return res.json();
+  } catch {
+    throw new Error(`Failed to fetch data from ${endpoint}`);
+  }
+}
 
 export default async function Home() {
-  // TEMP: Fetching the data from the JSON file
-  // const dictionaryData = await getDictionaryData();
+  try {
+    const dictionaryData = await getDictionaryData();
 
-  return (
-    <>
-      <h1 className="sr-only">Search Page.</h1>
-      <Search dictionaryData={dictionaryData} />
-    </>
-  );
+    return (
+      <>
+        <h1 className="sr-only">Search Page.</h1>
+        <Search dictionaryData={dictionaryData} />
+      </>
+    );
+  } catch {
+    return (
+      <div className="text-center">
+        <h1 className="text-lg font-bold">Error</h1>
+        <p>Failed to fetch data from the API.</p>
+      </div>
+    );
+  }
 }
