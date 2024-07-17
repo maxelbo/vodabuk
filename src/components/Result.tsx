450db3f57101d7ba9bf7e2153993a837001bff63
Max Elbo <hello.maxelbo@gmail.com>
2024-07-18 05:56:05 +0900

    feat(lang):filter lang

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,29 +1,35 @@
 import { Badge } from '@/components/ui/badge';
-import { DictionaryEntryType } from '@/lib/types';
-// TODO: move capitalizer to utils
-// import { capitalizer } from '@/lib/utils';
+import { capitalize } from '@/lib/utils';
+import type { DictionaryEntryType, LangContextProps } from '@/lib/types';
+import { useLangContext } from '@/context/LanguageContext';
 
-export function capitalizer(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+interface ResultProps {
+  result: DictionaryEntryType;
 }
 
-export default function Result({ result }: { result: DictionaryEntryType }) {
+export default function Result({ result }: ResultProps) {
+  const { lang: translationLang } = useLangContext() as LangContextProps;
+  const filteredTranslations = result.translations.filter(
+    (translation) => translation.lang === translationLang,
+  );
+  const filteredExamples = result.examples?.filter((example) => example.lang === translationLang);
+
   return (
     <div className="border-b-2 border-white py-4 text-left last:border-b-0">
       <div className="flex justify-between gap-5 pb-1">
-        <span className="text-xl font-bold">{capitalizer(result.word)}</span>
+        <span className="text-xl font-bold">{capitalize(result.word)}</span>
         <Badge className="flex min-w-10 justify-center italic">{result.category}</Badge>
       </div>
       <div>
-        {result.translations.map((translation) => capitalizer(translation.text)).join(', ')}
+        {filteredTranslations.map((translation) => capitalize(translation.text)).join(', ')}
       </div>
-      {result.examples && result.examples.length > 0 && (
+      {filteredExamples && filteredExamples.length > 0 && (
         <div>
           <p className="font-bold">Examples:</p>
           <ul className="list-inside">
-            {result.examples.map((example, i) => (
+            {filteredExamples.map((example, i) => (
               <li key={i} className="pl-4">
-                <span>{example.text}</span>
+                <span>{capitalize(example.text)}</span>
               </li>
             ))}
           </ul>
