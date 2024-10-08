2ab5abe54b2f1af41f25392e7d3d297672084ede
Max Elbo <hello.maxelbo@gmail.com>
2024-10-09 06:04:33 +0900

    refactor:use ternary ops in attrs instead of getters

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -8,16 +8,12 @@ const languageList = ['english', 'esperanto'];
 export default function LanguageSelect() {
   const { lang, setLang } = useLangContext() as LangContextProps;
 
-  function getLabel() {
-    return lang === 'english' ? 'Select a language.' : 'Elektu lingvon.';
-  }
-
   return (
     <select
       className="rounded-md px-4 py-1 capitalize"
       value={lang}
       onChange={(e) => setLang(e.target.value)}
-      aria-label={getLabel()}
+      aria-label={lang === 'english' ? 'Select a language.' : 'Elektu lingvon.'}
     >
       {languageList.map((lang, i) => (
         <option key={i} value={lang}>
