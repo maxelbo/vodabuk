4f4c42b6e9564fab191fb13988d3ba9fc1fd08cc
Max Elbo <hello.maxelbo@gmail.com>
2025-06-11 05:53:15 +0900

    feat(utils): add translation custom hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { useLangContext } from '@/context/LanguageContext';
+import type { LangContextProps } from '@/lib/types';
+import { ReactNode } from 'react';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,4 +14,14 @@ export function capitalize(string: string | null | undefined): string {
   const trimmed = string.trim();
 
   return trimmed.charAt(0).toUpperCase() + trimmed.slice(1);
+}
+
+export function useTranslation() {
+  const { lang } = useLangContext() as LangContextProps;
+
+  function translate<T extends string | ReactNode>(translations: Record<string, T>): T {
+    return translations[lang] || translations.volapük;
+  }
+
+  return { translate, lang };
 }
