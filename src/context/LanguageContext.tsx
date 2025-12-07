09c6d4ca9ce70fcfe58020c19099985eab5686c8
Max Elbo <hello.maxelbo@gmail.com>
2025-12-07 15:52:30 +0900

    fix: return Lang type

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { createContext, useContext, useState } from 'react';
-import type { LangContextProps, Children } from '@/lib/types';
+import type { Lang, LangContextProps, Children } from '@/lib/types';
 
 const LangContext = createContext<LangContextProps | undefined>(undefined);
 
 export function LanguageProvider({ children }: Children) {
-  const [lang, setLang] = useState('english');
+  const [lang, setLang] = useState<Lang>('english');
 
   return <LangContext.Provider value={{ lang, setLang }}>{children}</LangContext.Provider>;
 }
